3f71af67277cebf5d71ea47e092b971f45fddf19
Chen Levi <levichen24@gmail.com>
2022-12-21 08:36:01 +0200

    add backend env

--- frontend/src/Api/ApiController.js
+++ frontend/src/Api/ApiController.js
@@ -1,17 +1,18 @@
 import axios from "axios";
+import { Config } from "../config";
 
 export async function getStudents() {
-  let students = await axios.get("http://localhost:3000/students");
+  let students = await axios.get(`${Config.backendUrl}/studentds`,{ crossdomain: true });
   return students.data;
 }
 
 export async function addStudent(student) {
-  let res = await axios.post("http://localhost:3000/students", student);
+  let res = await axios.post(`${Config.backendUrl}/student`, student);
   return true;
 }
 
 export async function deleteStudent(studentId) {
-  let students = await axios.delete("http://localhost:3000/students", {
+  let students = await axios.delete(`${Config.backendUrl}/students`, {
     data: { studentId: studentId },
   });
   return true;
