2f451f3e291373b26720a30986a922fa636ee4f4
Assaf Yacobi <assafushy@gmail.com>
2023-01-08 15:25:21 +0200

    removed warnings

--- frontend/src/Api/ApiController.js
+++ frontend/src/Api/ApiController.js
@@ -9,12 +9,12 @@ export async function getStudents() {
 }
 
 export async function addStudent(student) {
-  let res = await axios.post(`${backendUrl}/students`, student);
+  await axios.post(`${backendUrl}/students`, student);
   return true;
 }
 
 export async function deleteStudent(studentId) {
-  let students = await axios.delete(`${backendUrl}/students`, {
+  await axios.delete(`${backendUrl}/students`, {
     data: { studentId: studentId },
   });
   return true;
